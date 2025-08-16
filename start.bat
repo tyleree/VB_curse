b6374a98eeb1fbccb49ec55f6e0b89ca72d27d68
tyleree <hcotyler@gmail.com>
2025-08-15 22:57:43 -0700

    Add new features and fix bugs

---
+++ start.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Starting Veterans Benefits Assistant...
+echo.
+echo Make sure you have:
+echo 1. Python installed
+echo 2. Dependencies installed (pip install -r requirements.txt)
+echo 3. Environment variables set in env.txt
+echo.
+echo Starting Flask server...
+python app.py
+pause
+
+
+
+
+
+
+
+
